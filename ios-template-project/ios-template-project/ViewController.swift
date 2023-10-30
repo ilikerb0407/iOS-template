1e899bac2dc8168c9c7a6b8ef45699f7294aede4
KaiFu <kevin12342247@gmail.com>
2023-10-31 01:08:37 +0800

    Create Template

---
+++ ios-template-project/ios-template-project/ViewController.swift
@@ -0,0 +1,17 @@
+/// <#Brief Description#> 
+///
+/// Created by TWINB00591630 on 2023/10/31.
+/// Copyright © 2023 Cathay United Bank. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-template-project/ios-template-projectUITests/ios_template_projectUITestsLaunchTests.swift
@@ -0,0 +1,30 @@
+/// <#Brief Description#> 
+///
+/// Created by TWINB00591630 on 2023/10/31.
+/// Copyright © 2023 Cathay United Bank. All rights reserved.
+
+import XCTest
+
+final class ios_template_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
